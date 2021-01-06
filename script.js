894c57b0f677714f34b68d343e078e2ec9084e8c
chrismcgale <56483395+chrismcgale@users.noreply.github.com>
2021-01-06 15:24:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const space = '500px';
 let colour = 'black';
 var canvas, ctx, flag = false,
+    prevX = 0,
+        currX = 0,
+        prevY = 0,
+        currY = 0,
+        dot_flag = false;
+
+ var x = "black",
+        y = 2;
 
 
 function init() {
@@ -56,6 +64,36 @@ function createButtons(){
 
 createButtons();
 
+function findxy(res, e) {
+        if (res == 'down') {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+    
+            flag = true;
+            dot_flag = true;
+            if (dot_flag) {
+                ctx.beginPath();
+                ctx.fillStyle = x;
+                ctx.fillRect(currX, currY, 2, 2);
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+}
 
 function sketch() {
         ctx.beginPath();
