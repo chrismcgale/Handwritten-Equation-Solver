61229e2f390e0c7a22118faa616d280dde590b7f
chrismcgale <56483395+chrismcgale@users.noreply.github.com>
2021-01-06 15:19:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,28 @@
 const space = '500px';
 let colour = 'black';
+ var canvas, ctx, flag = false,
 
 
-function createGrid(num){
-    const container = document.querySelector('#container');
-    var i = 0;
-    while (i < num){
-        const row = document.createElement('div');
-        row.setAttribute('class', 'row');
-        var j = 0;
-        while (j < num){
-            const col = document.createElement('div');
-            const px = Math.round(500 / num);
-
-            col.style.height = px.toString(10)+'px';
-
-            col.style.width = px.toString(10)+'px';
-            col.style.borderTop = "thin solid black";
-            col.style.borderLeft = "thin solid black";
-            col.style.cssFloat = "left";
-            col.setAttribute('class', 'cell');
-            row.setAttribute('class', 'col');
-            if (j === num -1){
-                col.style.borderRight = "thin solid black";
-            }
-            if(i === num - 1){
-                col.style.borderBottom="thin solid black";
-            }
-            row.appendChild(col);
-            j++;
-
-
-
-
-        }
-        row.style.clear='left';
-        container.appendChild(row);
-        i++;
-    }
+function init() {
+        canvas = document.getElementById('can');
+        ctx = canvas.getContext("2d");
+        w = canvas.width;
+        h = canvas.height;
     
-   sketch();
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
 }
 
-createGrid(45);
-
-function sketch(){
-    let cells = document.querySelectorAll('.cell');
-    cells.forEach((cell) =>{
-        cell.addEventListener('mouseover', function(){
-            cell.style.backgroundColor = colour;
-        });
-    });
-}
-
-sketch();
 
 function createButtons(){
     const buts = document.querySelector('#btn_ctn');
@@ -87,72 +56,22 @@ function createButtons(){
 createButtons();
 
 
-function reSize() {
-    const resize = document.querySelector('.resize_btn');
-    resize.addEventListener('mousedown', function () {
-        let cells = document.querySelectorAll('.cell');
-        cells.forEach((cell) =>{
-            cell.remove();
-        });
-        var num = prompt("Enter n by n:", "16");
-        createGrid(num);
-
-    });
-
+function sketch() {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = x;
+        ctx.lineWidth = y;
+        ctx.stroke();
+        ctx.closePath();
 }
-reSize();
 
-
-function clear(){
-    const clr = document.querySelector('.clear_btn');
-    clr.addEventListener('mousedown', function(){
-        let cells = document.querySelectorAll('.cell');
-
-        cells.forEach((cell) =>{
-            cell.style.backgroundColor = 'transparent';
-        });
-
-
-    });
-
-}
-clear();
-
-function random(){
-    const ran = document.querySelector('.randomColour');
-    ran.addEventListener('mousedown', function () {
-        let dic =['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-        let i = 0;
-        let hex = '';
-        while(i < 6){
-            let r = Math.floor((Math.random() * 16) + 1);
-            hex = hex + dic[r-1];
-            i++;
+function clear() {
+        var m = confirm("Want to clear");
+        if (m) {
+            ctx.clearRect(0, 0, w, h);
+            document.getElementById("canvasimg").style.display = "none";
         }
-        colour = '#' + hex;
-        ran.style.backgroundColor = colour;
-
-    })
-}
-random();
-
-function black(){
-    const ran = document.querySelector('.black');
-    ran.addEventListener('mousedown', function () {
-        colour = 'black';
-        const r = document.querySelector('.randomColour');
-        r.style.backgroundColor = 'transparent';
-
-    });
 }
 
-black();
 
-function change(){
-    const ran = document.querySelector('.change');
-    ran.addEventListener('mousedown', function () {
-        colour = prompt("Enter a colour or hex code", "red of #aw12034");
-    });
-}
-
-change();
